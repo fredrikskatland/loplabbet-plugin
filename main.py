e64efdac6e7fed81cc50b4c800c2168331c40112
Fredrik Skatland <fredrikskatland@gmail.com>
2023-11-15 20:41:10 +0100

    Fixed serialization of document issue

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pickle
 import quart
 import quart_cors
 from quart import request
+import os
 
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
@@ -17,15 +18,26 @@ db = Chroma.from_documents(documents, embeddings)
 #db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 retriever = db.as_retriever()
 
+def convert_to_dict(result):
+    page_content = [doc.page_content for doc in result]
+    metadatas = [doc.metadata for doc in result]
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+    output = []
+    for i in range(len(page_content)):
+        output.append({'page_content': page_content[i], 'metadata': metadatas[i]})
+
+    return output
+
+app = quart_cors.cors(quart.Quart(__name__))
 
 @app.post("/retrieve")
 async def retrieve():
     request = await quart.request.get_json(force=True)
     query = request["query"]
     result = retriever.get_relevant_documents(query)
-    return quart.Response(response=json.dumps(result), status=200)
+    output = convert_to_dict(result)
+    # Convert Documents to 
+    return quart.Response(response=json.dumps(output), status=200)
 
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
@@ -45,4 +57,5 @@ def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5003))
+    app.run(debug=True, host="0.0.0.0", port=port)
