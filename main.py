04103d217ae1a984a3b3185ed5519411b0efd543
Fredrik Skatland <fredrikskatland@gmail.com>
2023-11-18 20:00:34 +0100

    Updated privacy policy

--- main.py
+++ main.py
@@ -59,7 +59,44 @@ async def privacy_policy():
     #return await quart.send_from_directory(directory='path_to_directory', filename='privacy_policy.html')
 
     # Method 2: Return a string
-    privacy_text = "Your Privacy Policy Text Here"
+    privacy_text = """
+    Privacy Policy for the LöplabbetGPT API calls
+
+Last Updated: 2023-11-18
+
+1. Introduction
+
+Welcome to the LöplabbetGPT API. We are committed to respecting your privacy and protecting your personal information. This Privacy Policy explains the types of information we collect through our API and how we use this information.
+
+2. Information We Collect
+
+The LöplabbetGPT API is designed to collect only the information that is necessary to provide our service. The only data we collect is:
+
+Input Data: This includes the query argument that you provide when you make an API call. We use this data solely for the purpose of processing your request and providing you with the relevant response.
+3. Use of Collected Information
+
+The information we collect is used in the following ways:
+
+To process and respond to the queries received via the API.
+To improve the functionality and performance of our API.
+We do not use the information collected for any other purpose, nor do we share it with any third parties.
+
+4. Data Retention
+
+We retain the collected data only as long as necessary to provide the requested service. Once the purpose of data collection is fulfilled, we ensure the data is securely deleted from our systems.
+
+5. Security
+
+We take the security of your data seriously and implement appropriate technical and organizational measures to protect it against unauthorized or unlawful access, alteration, disclosure, or destruction.
+
+6. Changes to This Privacy Policy
+
+We may update this Privacy Policy from time to time. We encourage you to review this policy periodically to stay informed about how we are protecting the information we collect.
+
+7. Contact Us
+
+If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at [Your Contact Information].
+"""
     return quart.Response(response=privacy_text, status=200, mimetype='text/plain')
 
 
